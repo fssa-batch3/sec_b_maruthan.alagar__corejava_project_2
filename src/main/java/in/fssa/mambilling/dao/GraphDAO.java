362a898096bf33ae89a43c7c381d8a56b1576062
maruthu001 <maruthan.alagar@fwsa.freshworks.com>
2023-09-17 19:05:43 +0530

    Update GraphDAO.java

--- src/main/java/in/fssa/mambilling/dao/GraphDAO.java
+++ src/main/java/in/fssa/mambilling/dao/GraphDAO.java
@@ -26,67 +26,88 @@ public class GraphDAO {
 	 *                              details, a {@code PersistanceException} is
 	 *                              thrown.
 	 */
-	public  Map<String, Double> findGraphDetails() throws PersistanceException {
-		Connection con = null;
-		PreparedStatement ps = null;
-		ResultSet rs = null;
-		Map<String, Double> dateTotalAmounts = null;
-		try {
+	public Map<String, Double> findGraphDetails() throws PersistanceException {
+	    Map<String, Double> dateTotalAmounts = new HashMap<>();
+	    Connection con = null;
+	    PreparedStatement ps = null;
+	    ResultSet rs = null;
 
-			String sql = "SELECT DISTINCT DATE(timestamp) AS bill_date FROM bills";
-			con = ConnectionUtil.getConnection();
-			ps = con.prepareStatement(sql);
-			rs = ps.executeQuery();
-			dateTotalAmounts = new HashMap<>();
+	    try {
+	        String sql = "SELECT DISTINCT DATE(timestamp) AS bill_date FROM bills";
+	        con = ConnectionUtil.getConnection();
+	        ps = con.prepareStatement(sql);
+	        rs = ps.executeQuery();
 
-			while (rs.next()) {
-				String dateToQuery = rs.getString("bill_date");
-				double totalAmount = 0.0;
+	        while (rs.next()) {
+	            String dateToQuery = rs.getString("bill_date");
+	            double totalAmount = 0.0;
+	            PreparedStatement billIdsStmt = null;
+	            ResultSet billIdsResult = null;
 
-				// Retrieve bill_ids for the current date
-				String billIdsSql = "SELECT id FROM bills WHERE DATE(timestamp) = ?";
-				PreparedStatement billIdsStmt = con.prepareStatement(billIdsSql);
-				billIdsStmt.setString(1, dateToQuery);
+	            try {
+	                // Retrieve bill_ids for the current date
+	                String billIdsSql = "SELECT id FROM bills WHERE DATE(timestamp) = ?";
+	                billIdsStmt = con.prepareStatement(billIdsSql);
+	                billIdsStmt.setString(1, dateToQuery);
+	                billIdsResult = billIdsStmt.executeQuery();
 
-				ResultSet billIdsResult = billIdsStmt.executeQuery();
+	                while (billIdsResult.next()) {
+	                    String billId = billIdsResult.getString("id");
+	                    PreparedStatement billItemsStmt = null;
+	                    ResultSet billItemsResult = null;
 
-				while (billIdsResult.next()) {
-					String billId = billIdsResult.getString("id");
+	                    try {
+	                        // Retrieve bill_items and calculate total amount for each bill
+	                        String billItemsSql = "SELECT bi.quantity, p.mrp, p.tax, p.discount FROM bill_items bi INNER JOIN price p ON bi.product_id = p.product_id WHERE bi.bill_id = ?";
+	                        billItemsStmt = con.prepareStatement(billItemsSql);
+	                        billItemsStmt.setString(1, billId);
+	                        billItemsResult = billItemsStmt.executeQuery();
 
-					// Retrieve bill_items and calculate total amount for each bill
-					String billItemsSql = "SELECT bi.quantity, p.mrp, p.tax, p.discount FROM bill_items bi INNER JOIN price p ON bi.product_id = p.product_id WHERE bi.bill_id = ?";
-					PreparedStatement billItemsStmt = con.prepareStatement(billItemsSql);
-					billItemsStmt.setString(1, billId);
+	                        while (billItemsResult.next()) {
+	                            int quantity = billItemsResult.getInt("quantity");
+	                            double mrp = billItemsResult.getDouble("mrp");
+	                            double tax = billItemsResult.getDouble("tax");
+	                            double discount = billItemsResult.getDouble("discount");
 
-					ResultSet billItemsResult = billItemsStmt.executeQuery();
-					while (billItemsResult.next()) {
-						int quantity = billItemsResult.getInt("quantity");
-						double mrp = billItemsResult.getDouble("mrp");
-						double tax = billItemsResult.getDouble("tax");
-						double discount = billItemsResult.getDouble("discount");
+	                            // Calculate the price for each product and add to total
+	                            double taxAmount = (mrp * tax / 100.0);
+	                            double discountAmount = (mrp * discount / 100.0);
+	                            double price = (mrp + taxAmount - discountAmount) * quantity;
+	                            totalAmount += price;
+	                        }
+	                    } finally {
+	                        if (billItemsResult != null) {
+	                            billItemsResult.close();
+	                        }
+	                        if (billItemsStmt != null) {
+	                            billItemsStmt.close();
+	                        }
+	                    }
+	                }
+	            } finally {
+	                if (billIdsResult != null) {
+	                    billIdsResult.close();
+	                }
+	                if (billIdsStmt != null) {
+	                    billIdsStmt.close();
+	                }
+	            }
 
-						// Calculate the price for each product and add to total
-						double taxAmount = (mrp * tax / 100.0);
-						double discountAmount = (mrp * discount / 100.0);
-						double price = (mrp + taxAmount - discountAmount) * quantity;
-						totalAmount += price;
-					}
-				}
+	            // Store the total amount for the current date
+	            dateTotalAmounts.put(dateToQuery, totalAmount);
+	        }
 
-				// Store the total amount for the current date
-				dateTotalAmounts.put(dateToQuery, totalAmount);
-			}
+	        for (Map.Entry<String, Double> entry : dateTotalAmounts.entrySet()) {
+	            System.out.println("Date: " + entry.getKey() + ", Total Amount: " + entry.getValue());
+	        }
 
-			for (Map.Entry<String, Double> entry : dateTotalAmounts.entrySet()) {
-				System.out.println("Date: " + entry.getKey() + ", Total Amount: " + entry.getValue());
-			}
-
-			return dateTotalAmounts;
-		} catch (SQLException e) {
-			throw new PersistanceException(e.getMessage());
-		} finally {
-			ConnectionUtil.close(con, ps, rs);
-		}
+	        return dateTotalAmounts;
+	    } catch (SQLException e) {
+	        throw new PersistanceException(e.getMessage());
+	    } finally {
+	        ConnectionUtil.close(con, ps, rs);
+	    }
 	}
+
 
 }
